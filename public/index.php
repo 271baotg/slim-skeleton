aa160f2c0eb3cf6c5dc7ad68e27a7f33b752029e
271baotg <transformers2712003@gmail.com>
2024-11-26 15:10:51 +0700

    Create custom list response for pagination + Add JwtMiddleware

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use App\Application\Handlers\HttpErrorHandler;
 use App\Application\Handlers\ShutdownHandler;
+use App\Application\Middleware\JwtMiddleware;
 use App\Application\ResponseEmitter\ResponseEmitter;
 use App\Application\Settings\SettingsInterface;
 use DI\ContainerBuilder;
@@ -41,16 +42,25 @@ AppFactory::setContainer($container);
 $app = AppFactory::create();
 $callableResolver = $app->getCallableResolver();
 
-// Register middleware
-$middleware = require __DIR__ . '/../app/middleware.php';
-$middleware($app);
-
 // Register routes
 $routes = require __DIR__ . '/../app/routes.php';
 $routes($app);
 
 /** @var SettingsInterface $settings */
 $settings = $container->get(SettingsInterface::class);
+
+// Register the secret key in the container (use config or environment variable)
+$container->set('jwt.secret_key', $settings->get('jwt.secret_key') ?? getenv('JWT_SECRET_KEY'));
+
+// Register the JwtMiddleware in the container
+$container->set(JwtMiddleware::class, function ($container) {
+    $secretKey = $container->get('jwt.secret_key');
+    return new JwtMiddleware($secretKey);
+});
+
+// Register middleware
+$middleware = require __DIR__ . '/../app/middleware.php';
+$middleware($app);
 
 $displayErrorDetails = $settings->get('displayErrorDetails');
 $logError = $settings->get('logError');
@@ -70,11 +80,7 @@ $responseFactory = $app->getResponseFactory();
 $errorHandler = new HttpErrorHandler($callableResolver, $responseFactory);
 
 // Create Shutdown Handler
-$shutdownHandler = new ShutdownHandler(
-    $request,
-    $errorHandler,
-    $displayErrorDetails,
-);
+$shutdownHandler = new ShutdownHandler($request, $errorHandler, $displayErrorDetails);
 register_shutdown_function($shutdownHandler);
 
 // Add Routing Middleware
@@ -84,11 +90,7 @@ $app->addRoutingMiddleware();
 $app->addBodyParsingMiddleware();
 
 // Add Error Middleware
-$errorMiddleware = $app->addErrorMiddleware(
-    $displayErrorDetails,
-    $logError,
-    $logErrorDetails,
-);
+$errorMiddleware = $app->addErrorMiddleware($displayErrorDetails, $logError, $logErrorDetails);
 $errorMiddleware->setDefaultErrorHandler($errorHandler);
 
 // Run App & Emit Response
